0ae5b8cfe1d207696489462892c0ef5fac2346c9
Giles Thomas <giles@giles.net>
2026-07-27 18:53:56 +0100

    Simple training loop, a model that works

--- non_moe_model.py
+++ non_moe_model.py
@@ -7,10 +7,13 @@ class NonMoEModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.layer_1 = nn.Linear(2, 4)
-        self.layer_2 = nn.Linear(4, 1)
+        self.layer_2 = nn.Linear(4, 4)
+        self.layer_3 = nn.Linear(4, 1)
 
 
     def forward(self, xs):
         xs = F.relu(self.layer_1(xs))
-        return F.relu(self.layer_2(xs))
+        xs = F.relu(self.layer_2(xs))
+        xs = F.relu(self.layer_3(xs))
+        return xs
 
--- train.py
+++ train.py
@@ -1,8 +1,83 @@
+import random
+import time
+
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader, Dataset
+
 from non_moe_model import NonMoEModel
 
 
+class MyDataset(Dataset):
+
+    def __init__(self, filename):
+        self.raw_data = torch.tensor(
+            pd.read_csv(filename, header=None).values,
+            dtype=torch.float
+        )
+
+
+    def __len__(self):
+        return len(self.raw_data)
+
+
+    def __getitem__(self, ix):
+        row = self.raw_data[ix]
+        return (
+            row[:2],
+            row[2],
+        )
+
+
+BATCH_SIZE = 64
+EPOCHS = 3000
+LR = 1e-2
+WEIGHT_DECAY = 0
+
+
+def calculate_loss(results, targets):
+    return ((results - targets) ** 2).mean()
+
+
+def train(model, dataloader):
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=LR, weight_decay=WEIGHT_DECAY
+    )
+
+    start = time.time()
+    for epoch in range(EPOCHS):
+        losses = []
+        for xs, ys in dataloader:
+            optimizer.zero_grad()
+
+            results = model(xs).squeeze()
+
+            loss = calculate_loss(results, ys)
+            loss.backward()
+            losses.append(loss.item())
+
+            optimizer.step()
+        avg_loss = sum(losses) / len(losses)
+        print(f"Epoch {epoch + 1}: loss is {avg_loss:.2f}")
+    end = time.time()
+
+    print(f"Trained in {end - start:.1f}s, final loss {avg_loss:.2f}")
+
+
 def main():
+    seed = 42
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    dataset = MyDataset("data.csv")
+    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
     model = NonMoEModel()
+
+    train(model, dataloader)
 
 
 if __name__ == "__main__":
